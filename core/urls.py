aa1ec1f23fd08cd2a45406b464ec4cd95fcefec7
husanibragimov <ibragmovhusan283@gmail.com>
2024-07-20 22:33:30 +0500

    image and static

--- core/urls.py
+++ core/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     # prefix_default_language=False
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'core.handler.handler404'
