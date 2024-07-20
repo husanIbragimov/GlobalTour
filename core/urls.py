911e1f939d0156e459e78c1fbc7f05f64ac85146
husanibragimov <ibragmovhusan283@gmail.com>
2024-07-20 22:41:46 +0500

    static path error

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,8 +34,9 @@ urlpatterns = [
     # prefix_default_language=False
 )
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
 
 handler404 = 'core.handler.handler404'
