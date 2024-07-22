abf6ad27798878dff05ef306537c646f292d179d
husanibragimov <ibragmovhusan283@gmail.com>
2024-07-22 23:16:46 +0500

    Remove Group

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import Country
 
@@ -7,3 +8,4 @@ admin.site.site_title = 'Global Tour Site Admin'
 admin.site.index_title = 'Global Tour Admin Panel'
 
 admin.site.register(Country)
+admin.site.unregister(Group)
